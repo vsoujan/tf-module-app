67dcac7c2bfed1f2c718521c741ab3637d3eb2e6
soujanvangapandu <vangapandusoujan6@gmail.com>
2023-09-22 01:10:42 +0530

    ansible

--- main.tf
+++ main.tf
@@ -34,9 +34,11 @@ resource "aws_launch_template" "main" {
   image_id      = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [aws_security_group.main.id]
+
   user_data = base64encode(templatefile("${path.module}/userdata.sh",
     {
       component = var.component
+      env       = var.env
     }
   ))
 
--- userdata.sh
+++ userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-yum install ansible -y &>>/opt/userdata.log
-ansible-pull -i localhost, -U https://github.com/vsoujan/roboshop-ansible.git main.yml -e component=${component} &>>/opt/userdata.log
+yum install ansible python3.11-pip.noarch -y &>>/opt/userdata.log
+pip3.11 install botocore boto3 &>>/opt/userdata.log
+ansible-pull -i localhost, -U https://github.com/vsoujan/roboshop-ansible.git main.yml component=${component} -e env=${env} &>>/opt/userdata.log
